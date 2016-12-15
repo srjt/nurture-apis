58669f65b50939e064cd78abbdf153b8e8a33379
surjits <surjits@vmware.com>
2016-12-14 19:40:25 -0800

    initial commit

---
+++ typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/express-serve-static-core/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/body-parser/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/mime/index.d.ts" />
+/// <reference path="modules/mongoose/index.d.ts" />
+/// <reference path="modules/serve-static/index.d.ts" />
